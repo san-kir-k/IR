5feaaf40ed581b3fb0775f69a2ba05c9a5d06e13
san-kir-k <dfomka@list.ru>
2022-12-22 23:36:35 +0300

    Add http server

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,22 +1,51 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate futures;
+
+lazy_static! {
+    static ref ENGINE: engine::Engine = futures::executor::block_on(engine::init_engine()).unwrap();
+}
+
 pub mod db;
 pub mod engine;
 pub mod index;
 pub mod inverted_index;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let engine = engine::init_engine().await?;
-    let query = vec![
-        "relief".to_string(),
-        "pitcher".to_string(),
-        "game".to_string(),
-        "pitch".to_string(),
-    ];
-    let res = engine.search(query).await?;
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
-    for oid in res {
+#[derive(Debug, Serialize, Deserialize)]
+struct Request {
+    words: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    doc_ids: Vec<[u8; 12]>,
+}
+
+#[post("/search")]
+async fn search(req_body: String) -> impl Responder {
+    println!("Request: {req_body}");
+    let data: Request = serde_json::from_str(&req_body).unwrap();
+    let result = ENGINE.search(data.words).await.unwrap();
+
+    print!("Found doc_ids:\n");
+    for oid in &result {
         print!("{:?}\n", oid.to_hex());
     }
 
-    Ok(())
+    let response = Response {
+        doc_ids: result.iter().map(|oid| oid.bytes()).collect(),
+    };
+
+    return HttpResponse::Ok().body(serde_json::to_string(&response).unwrap());
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(search))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
 }
