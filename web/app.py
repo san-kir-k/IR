924206591b4f1b8eb9e87750c2c8648f8fb18d52
san-kir-k <dfomka@list.ru>
2022-12-29 23:22:37 +0300

    add 'search original request flag' to web

--- web/app.py
+++ web/app.py
@@ -12,6 +12,7 @@ import logging
 import asyncio
 import httpx
 import time
+import ast
 
 
 loop = asyncio.get_event_loop()
@@ -24,44 +25,59 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = Settings().secret
 
 
-@app.route('/', methods=('GET', 'POST'))
-def search():
+@app.route('/result/<search_engine_request>&<enriched_request>', methods=('POST', 'GET'))
+def result(search_engine_request=None, enriched_request=None):
+    if type(search_engine_request) is str:
+        search_engine_request = ast.literal_eval(search_engine_request)
+    if type(enriched_request) is str:
+        enriched_request = ast.literal_eval(enriched_request)
+
+    start = time.time()
+    search_engine_response = httpx.post('http://localhost:8080/search',
+                                        json={'words': search_engine_request},
+                                        timeout=None)
+    end = time.time()
+    logger.debug("Search engine request time [sec]: %s", end - start)
+
+    doc_ids: List = search_engine_response.json()["doc_ids"]
+    logger.debug("Got %s documents in search engine", len(doc_ids))
+    results = loop.run_until_complete(get_documents(doc_ids))
+    logger.debug("Response: %s", results)
+
+    is_changed: bool = False
+    front_request: List[Dict] = []
+    for given, changed in zip(enriched_request, search_engine_request):
+        if given != changed:
+            front_request.append({"word": changed, "color": "red"})
+            is_changed = True
+        else:
+            front_request.append({"word": changed, "color": "black"})
+
+    return render_template('result.html',
+                           results=results, changed_request=front_request,
+                           show_hidden=is_changed, original_request=enriched_request)
+
+
+@app.route('/', methods=('POST', 'GET'))
+def search(search_request=None):
     if request.method == 'POST':
         search_request = request.form['request']
-        enriched_request: List[str] = get_enriched_words(search_request)
-
-        bi: BigramIndex = BigramIndex(enriched_request)
-        loop.run_until_complete(bi.build(logger))
-
-        search_dict: Dict = bi.get_search_dict()
-        logger.debug("Supposed request structure: %s", search_dict)
-
-        search_engine_request: List = [supposed[0] for supposed in search_dict.values()]
-        logger.debug("Request for search engine: %s", search_engine_request)
-
-        start = time.time()
-        search_engine_response = httpx.post('http://localhost:8080/search',
-                                            json={'words': search_engine_request},
-                                            timeout=None)
-        end = time.time()
-        logger.debug("Search engine request time [sec]: %s", end - start)
-
-        doc_ids: List = search_engine_response.json()["doc_ids"]
-        logger.debug("Got %s documents in search engine", len(doc_ids))
-        results = loop.run_until_complete(get_documents(doc_ids))
-        logger.debug("Response: %s", results)
-
-        front_request: List[Dict] = []
-        for given, changed in zip(enriched_request, search_engine_request):
-            if given != changed:
-                front_request.append({"word": changed, "color": "red"})
-            else:
-                front_request.append({"word": changed, "color": "black"})
-
         if not search_request:
             flash('Request is required!')
         else:
-            return render_template('result.html', results=results, changed_request=front_request)
+            enriched_request: List[str] = get_enriched_words(search_request)
+
+            bi: BigramIndex = BigramIndex(enriched_request)
+            loop.run_until_complete(bi.build(logger))
+
+            search_dict: Dict = bi.get_search_dict()
+            logger.debug("Supposed request structure: %s", search_dict)
+
+            search_engine_request: List = [supposed[0] for supposed in search_dict.values()]
+            logger.debug("Request for search engine: %s", search_engine_request)
+
+            return result(search_engine_request=search_engine_request, enriched_request=enriched_request)
+
     return render_template('search.html')
 
 
